e118a52d698d5bf37088a2ee3722c794a9996c91
DILAN ALVARO ALAVI GUTIERREZ <dilanalavi11@gmail.com>
2025-04-27 07:02:49 -0400

    Creamos la interfaz para la api de meal db

---
+++ framework/src/main/java/com/ucb/framework/service/IMealApiService.kt
@@ -0,0 +1,11 @@
+package com.ucb.framework.service
+
+import com.ucb.framework.dto.MealResponseDto
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface IMealApiService {
+    @GET("/api/json/v1/1/search.php")
+    suspend fun searchMealByName(@Query("s") mealName: String): Response<MealResponseDto>
+}
