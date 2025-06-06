2b237dc157a43e9854bcd38d706a110a97e1b811
A-Joaquin <arturo.veliz@ucb.edu.bo>
2025-06-06 00:27:13 -0400

    conflictos con main fix

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -15,5 +15,4 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation(libs.kotlinx.coroutines.core)
     implementation("javax.inject:javax.inject:1")
-
 }
